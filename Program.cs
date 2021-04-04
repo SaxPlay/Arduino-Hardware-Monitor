7dfaf24f9cb341966e9cfa6059796f5ab5787381
SaxPlay <81904161+SaxPlay@users.noreply.github.com>
2021-04-04 07:22:57 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,9 +1,29 @@
-﻿using System;
+﻿/* README
+ * 
+* First of all go to the Open Hardware monitor page, download and install the program because you will need the OpenHardwareMonitorLib.dll file. 
+* https://openhardwaremonitor.org/downloads/
+* I have uploaded a copy of this DLL to the repository but maybe on the web site there is a more updated version.
+* Important: You must add this library as a reference to the project you must also add System (from .NET libraries) as a reference.
+* -> If you do not add these references the code will not work at all.  <-
+* 
+* If you don't know how to add a reference you can follow the following tutorial:
+* https://docs.microsoft.com/en-us/visualstudio/ide/managing-references-in-a-project?view=vs-2019
+* 
+* Feel free to improve and adapt the code according to your criteria and needs.
+* 
+* I got the original idea from the following blog. It was very useful for me.
+* https://www.hackster.io/haoming-weng/get-cpu-temperature-using-open-hardware-monitor-in-c-1a3338
+* 
+* That's all
+* Have a nice day
+* 04/04/2021
+*/
+using System;
 using OpenHardwareMonitor.Hardware;
 using System.Threading;
 using System.IO.Ports;
 
-namespace Get_CPU_Temp5
+namespace Get_Hardware_Info
 {
     class Program
     {
@@ -22,7 +42,7 @@ namespace Get_CPU_Temp5
             public void VisitParameter(IParameter parameter) { }
         }
 
-        /*
+        /* -> This may vary depending on the hardware type <-
         Sensors for Hardware[0]     CPU
             0 to 5 Individual Core Usage %
             6 average CPU Usage 
